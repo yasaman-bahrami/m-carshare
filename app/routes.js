0e0011b283b13027f7e5f8b50879d0e6ff344f54
yasaman bahrami samani <yasaman.bahrami@gmail.com>
2016-11-06 22:17:51 -0330

    --Edited, removed and moved some committed files to follow the structure of the project

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,9 @@
-
 var Car = require('../app/models/car');
 var CarType = require('../app/models/carType');
+var User = require('../app/models/user');
 // Email verification
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
-var User = require('./models/user');
 var emailTemplate = require('./emailTemplate');
 
 function sendMail(mailOptions, email, callback) {
@@ -18,12 +17,12 @@ function sendMail(mailOptions, email, callback) {
         }
     });
     // send mail with defined transport object
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
             console.log('Error occurred while sending email to ', email, '. Detailed Err: ', error);
             callback(error);
         }
-        else{
+        else {
             console.log('Message sent successfully to ', email);
             callback(null, response.message);
         }
@@ -69,30 +68,30 @@ module.exports = function (app, passport, mongoose) {
     });
 
     //Route to receive signup form data
-    app.get('/pages/verifyMe', function(req, res){
-        if(req.query==undefined || req.query.vc==undefined || req.query.vc==null) {
+    app.get('/pages/verifyMe', function (req, res) {
+        if (req.query == undefined || req.query.vc == undefined || req.query.vc == null) {
             return res.render('pages/verificationStatus.ejs', {});
         }
 
         var verificationCode = req.query.vc;
 
-        User.findOne({verificationCode: verificationCode}, function(err, user){
-            if(err) {
+        User.findOne({verificationCode: verificationCode}, function (err, user) {
+            if (err) {
                 console.log('ERROR occurred while fetching user corressponding to given verificationCode: ', verificationCode);
                 return res.render('pages/verificationStatus.ejs', {
                     message: "Verification link is invalid."
                 });
             }
-            else if(user) {
+            else if (user) {
                 console.log('User found=> ', user);
-                if(user.isEmailVerified){
+                if (user.isEmailVerified) {
                     return res.render('pages/verificationStatus.ejs', {
                         message: "Your email address is already verified"
                     });
                 }
                 else {
                     user.isEmailVerified = true;
-                    user.save(function(err){
+                    user.save(function (err) {
                         console.log("User email address verified now.");
                     });
                     return res.render('pages/verificationStatus.ejs', {
@@ -109,11 +108,11 @@ module.exports = function (app, passport, mongoose) {
         });
     });
 
-    app.post('/pages/register', function(req, res){
+    app.post('/pages/register', function (req, res) {
         var user = new User(req.body);
         user.verificationCode = crypto.createHash('sha1').update(user.email).digest('hex');
         console.log(user);
-        user.save(function(err) {
+        user.save(function (err) {
             if (err) {
                 switch (err.code) {
                     case 11000:
@@ -138,10 +137,10 @@ module.exports = function (app, passport, mongoose) {
                         }
                 }
             }
-            else{
-                var verificationLink = "http://localhost/pages/verifyMe?vc=" + user.verificationCode;
+            else {
+                var verificationLink = "http://" + req.headers.host + ":" + app.get('port') + "/pages/verifyMe?vc=" + user.verificationCode;
                 var mailOptions = emailTemplate.welcome_email(user.firstName, verificationLink);
-                sendMail(mailOptions, user.email, function(err) {
+                sendMail(mailOptions, user.email, function (err) {
                     console.log("Email sent");
                 });
                 res.json({success: "A confirmation mail has been sent to " + user.email});
@@ -155,7 +154,7 @@ module.exports = function (app, passport, mongoose) {
         });
     });
     app.get('/pages/addCar', function (req, res) {
-        Car.find().populate('carType').exec(function(err, cars) {
+        Car.find().populate('carType').exec(function (err, cars) {
             res.render('pages/addCar.ejs', {
                 user: req.user, // get the user out of session and pass to template
                 cars: cars
@@ -164,11 +163,11 @@ module.exports = function (app, passport, mongoose) {
 
     });
     app.post('/pages/addCar', function (req, res) {
-        CarType.find().where('_id').equals(req.body.carType).exec(function(err, carType) {
+        CarType.find().where('_id').equals(req.body.carType).exec(function (err, carType) {
             if (err) {
                 console.logx("Error occured while fetching carType");
                 res.send("ERROR IN find CarType");
-            }else{
+            } else {
                 var car = new Car();
                 car.carPlateNo = req.body.carPlateNumber;
                 car.carName = req.body.carName;
@@ -179,22 +178,22 @@ module.exports = function (app, passport, mongoose) {
                 car.longitude = req.body.carLongitude;
                 car.isAvailable = true;
                 car.carType = carType[0]._id;
-                if(req.body.carAvailable === undefined){
+                if (req.body.carAvailable === undefined) {
                     car.isAvailable = false;
-                }else{
+                } else {
                     car.isAvailable = true;
                 }
                 car.save(function (err, savedCar) {
                     if (err) {
                         console.log("This is the initialization message: " + JSON.stringify(err));
                         res.send("ERROR IN Car Save");
-                    }else{
-                        Car.findById(savedCar._id).populate('carType').exec(function(err, targetCar) {
-                           if(err){
-                               res.send("ERROR IN FindByID");
-                           } else{
-                               res.send(targetCar);
-                           }
+                    } else {
+                        Car.findById(savedCar._id).populate('carType').exec(function (err, targetCar) {
+                            if (err) {
+                                res.send("ERROR IN FindByID");
+                            } else {
+                                res.send(targetCar);
+                            }
                         });
 
                     }
