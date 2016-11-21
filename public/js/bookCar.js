9d5e27c72ca026d10b65f7321cf9d2bb91165c65
BinaJaved <bina.jkhan@gmail.com>
2016-11-21 10:42:16 -0330

    Add files via upload
    Bill functionality added in rentCarByModel.
    It is a new file.

---
+++ public/js/bookCar.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Bina on 2016-11-21.
+ */
+$('#bookCarForm').on('submit', function (e) {
+    e.preventDefault();
+    var carId = $('#carId').val();
+    $.ajax({
+        type: 'post',
+        url: '/pages/bookCar',
+        data: $('#bookCarForm').serialize(),
+        success: function (result) {
+            if (result == "ERROR") {
+                alert("A problem occured. Try again later.");
+            } else {
+                $("#modal-container-bill").modal('hide');
+                $("#modal-container-bill .form-group input").each(function () {
+                    $(this).val("");
+                });
+                $("#car-row-"+carId).remove();
+            }
+        },
+        error: function () {
+            alert("A problem occured. Try again later.");
+        }
+    });
+
+});
+
+function setCarId(carId) {
+    $('#bookCarForm').find('#carId').val(carId);
+}
