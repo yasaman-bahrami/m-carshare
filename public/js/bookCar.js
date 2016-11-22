b7657f91767834a08ff01fb6cf417f6b22a1048a
BinaJaved <bina.jkhan@gmail.com>
2016-11-21 21:48:11 -0330

    Add files via upload
    This is a new file
    Bill functionality added.
    Please merge this one.

---
+++ public/js/bookCar.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Bina on 2016-11-21.
+ */
+
+$('#bookCarForm').on('submit', function (e) {
+    e.preventDefault();
+    var carId = $('#carId').val();
+    $.ajax({
+        type: 'post',
+        url: '/pages/bookCar',
+        data: $('#bookCarForm').serialize(),
+        success: function (result) {
+            if (result == "ERROR") {
+                alert("A problem occured. Try again later.");
+            } else {
+                $("#modal-container-bill").modal('hide');
+                $("#modal-container-bill .form-group input").each(function () {
+                    $(this).val("");
+                });
+                $("#car-row-"+carId).closest('table').remove();
+            }
+        },
+        error: function () {
+            alert("A problem occured. Try again later.");
+        }
+    });
+
+});
+
+function setCarId(carId) {
+    $('#bookCarForm').find('#carId').val(carId);
+}
